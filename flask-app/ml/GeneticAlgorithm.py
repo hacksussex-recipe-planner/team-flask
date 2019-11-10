f9ca4e4a6830545b88a4f27412b6ebc03c10dda1
KacperKubara <kjk1u17@soton.ac.uk>
2019-11-10 03:57:42 +0000

    Model is tested and it is working

--- flask-app/ml/GeneticAlgorithm.py
+++ flask-app/ml/GeneticAlgorithm.py
@@ -2,7 +2,7 @@ import json
 import random
 
 from deap import base, creator, tools
-
+from deap.algorithms import eaSimple
 
 class DataLoader_Mixin():
     """ DataLoader_Mixin"""
@@ -24,26 +24,38 @@ class DataLoader_Mixin():
             dict_temp = json_file
         else: 
             dict_temp = json.load(json_file)
-
+        print(dict_temp)
         self.calories = dict_temp["calories"]
         self.proteins = dict_temp["proteins"]
         self.carbohydrates = dict_temp["carbohydrates"]
         self.fat = dict_temp["fat"]
         self.meals_per_day = dict_temp["meals_per_day"]
     
-    def data_sample(self):
+    def data_sample(self, ind):
         with open(self.file_path) as json_read:
             dict_temp = json.load(json_read)
             instance = []
             for i in range(self.meals_per_day):
                 key = random.sample(dict_temp.keys(), 1)[0]
-                print(key)
                 instance.append([
                     key, int(dict_temp[key]["calories"]),
                     int(dict_temp[key]["proteins"]), int(dict_temp[key]["carbohydrates"]),
                     int(dict_temp[key]["fat"])
                 ])
-            return instance
+            return ind(instance)
+
+    def data_sample_one(self):
+        with open(self.file_path) as json_read:
+            dict_temp = json.load(json_read)
+            instance = []
+            for i in range(self.meals_per_day):
+                key = random.sample(dict_temp.keys(), 1)[0]
+                instance.append([
+                    key, int(dict_temp[key]["calories"]),
+                    int(dict_temp[key]["proteins"]), int(dict_temp[key]["carbohydrates"]),
+                    int(dict_temp[key]["fat"])
+                ])
+            return instance[0]
 
 class GeneticAlgorithm(DataLoader_Mixin):
     """ Genetic Algorithm class"""
@@ -51,29 +63,88 @@ class GeneticAlgorithm(DataLoader_Mixin):
         super().__init__()
         self.ind_size = ind_size
         # 0, because ID needs to be stored
-        creator.create("FitnessMulti", base.Fitness, weights=(0, -1, -0.25, -0.25, -0.25, 
-                                                              0, -1, -0.25, -0.25, -0.25, 
-                                                              0, -1, -0.25, -0.25, -0.25))
+        creator.create("FitnessMulti", base.Fitness, weights=(0, -5, -0.5, -0.5, -0.5))
         creator.create("Individual", list, fitness=creator.FitnessMulti)
         
         self.toolbox = base.Toolbox()
         # Register functions
-        self.toolbox.register("attribute", super().data_sample)
+        self.toolbox.register("attribute", super().data_sample, ind=creator.Individual)
         self.toolbox.register("individual", tools.initRepeat, creator.Individual, 
                               self.toolbox.attribute, n=self.ind_size)
         self.toolbox.register("population", tools.initRepeat, list, self.toolbox.individual)
         
-        self.toolbox.register("mate", tools.cxOnePoint)
-        self.toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+        self.toolbox.register("mate", tools.cxOnePoint) # no need to change
+        self.toolbox.register("mutate", self._mutate, indpb=0.1)
+        self.toolbox.register("select", tools.selTournament, tournsize=3)
         self.toolbox.register("evaluate", self._evaluate)
 
     def run_algorithm(self, file_path: str, json_file) -> dict():
         super().data_load(file_path)
         super().model_input_load(json_file)
+        pop = self.toolbox.population(n=300)[0]      
+        CXPB, MUTPB, NGEN = 0.5, 0.2, 20        
+        # Evaluate the entire population
+        fitnesses = list(map(self.toolbox.evaluate, pop))
+        for ind, fit in zip(pop, fitnesses):
+            ind.fitness.values = fit   
+        print(f"fitnesses[0]: {fitnesses[0]}, fitnesses[1]: {fitnesses[1]}")
+        print(f"pop[0]: {pop[0]} \n pop[1]: {pop[1]}\n pop[0]>pop[1]?: {pop[0]>pop[1]}")
+        
+        for g in range(NGEN):
+            # Select the next generation individuals
+            offspring = self.toolbox.select(pop, len(pop))
+            #print(f"offspring: {offspring}")
+            #print(f"\npop: {pop}")
+            # Clone the selected individuals
+            offspring = list(map(self.toolbox.clone, offspring))
+
+            # Apply crossover and mutation on the offspring
+            for child1, child2 in zip(offspring[::2], offspring[1::2]):
+                if random.random() < CXPB:
+                    self.toolbox.mate(child1, child2)
+                    del child1.fitness.values
+                    del child2.fitness.values  
+
+            for mutant in offspring:
+                if random.random() < MUTPB:
+                    self.toolbox.mutate(mutant)
+                    del mutant.fitness.values
+            # Evaluate the individuals with an invalid fitness
+            invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+            fitnesses = map(self.toolbox.evaluate, invalid_ind)
+            for ind, fit in zip(invalid_ind, fitnesses):
+                ind.fitness.values = fit
+
+            # The population is entirely replaced by the offspring
+            pop[:] = offspring        
+
+        return pop
+    
+
+    def run_fake_algorithm(self, file_path: str, json_file) -> dict():
+        super().data_load(file_path)
+        super().model_input_load(json_file)
         keys = random.sample(list(self.data), self.meals_per_day)
         return [self.data[k] for k in keys]
         
-    def _mutate(self, instance, mu, sigma, indpb):
+    def _mutate(self, instance, indpb):
+        indpb = [indpb]
+        if len(indpb) == 1:
+            indpb = list(indpb) * self.meals_per_day
+        if len(indpb) != self.meals_per_day:
+            print("Weird mutation error!")
+            print(indpb)
+            indpb = indpb[:1] * self.meals_per_day
+            print(f"New probs: {indpb}")
+
+        for i, proba in enumerate(indpb):
+            rand_float = random.uniform(0, 1)
+            if rand_float <= proba:
+                instance_new = super().data_sample_one()
+                instance[i] = instance_new
+        return instance
+
+    def _select(self, individuals, k, tournsize):
         pass
 
     def _evaluate(self, instance):
@@ -82,7 +153,7 @@ class GeneticAlgorithm(DataLoader_Mixin):
         carbs = 0
         fats = 0
         # Compute a squared error
-        for i in range(self.meals_per_day):
+        for i in range(self.meals_per_day):                
             cals += instance[i][1]
             prots += instance[i][2]
             carbs += instance[i][3]
@@ -91,14 +162,42 @@ class GeneticAlgorithm(DataLoader_Mixin):
         prots = (self.proteins - prots) ** 2
         carbs = (self.carbohydrates - carbs) ** 2
         fats = (self.fat - fats) ** 2
-
-        return (cals, prots, carbs, fats)
+        return (0, cals, prots, carbs, fats)
 
 if __name__ == "__main__":
     ga = GeneticAlgorithm()
     data_gen = DataLoader_Mixin()
     json_file = data_gen.data_load("./sample.json", True)
-    print(ga.run_algorithm("./data.json", json_file))
+    result = ga.run_algorithm("./data.json", json_file)
+    result_arr = []
+    print(result)
+
+    test = []
+
+    for day in result:
+        results_arr_temp = {"calories" : 0, "proteins" : 0, "carbs" : 0, "fat" : 0}
+        for meal in day:
+            results_arr_temp["calories"] += meal[1]
+            results_arr_temp["proteins"] += meal[2]
+            results_arr_temp["carbs"] += meal[3]
+            results_arr_temp["fat"] += meal[4]
+        test.append(results_arr_temp)
+    print(test)
+
+    """for i, sub_arr in enumerate(result):
+        results_arr_temp = {"calories" : 0, "proteins" : 0, "carbs" : 0, "fat" : 0}
+        for j in range(ga.meals_per_day):
+            results_arr_temp["calories"] += sub_arr[j][1]
+            results_arr_temp["proteins"] += sub_arr[j][2]
+            results_arr_temp["carbs"] += sub_arr[j][3]
+            results_arr_temp["fat"] += sub_arr[j][4]
+ 
+        result_arr.append(results_arr_temp)"""
+    print(f"result_array: {result_arr}")
+    print(f"Desired intake: {[ga.calories, ga.proteins, ga.carbohydrates, ga.fat]}")
+    """
     for i in range(3):
         instance = ga.data_sample()
         print(f"Evaluation metric: {ga._evaluate(instance)}\n Run on: {instance}")
+        ga._mutate(instance, indpb=[0.1])
+    """
